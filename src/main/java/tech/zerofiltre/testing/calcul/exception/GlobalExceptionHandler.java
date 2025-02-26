caf8a8a57649f81cd2b9ef9d50b623819b337d51
Gilles Mbagou <gilles.boussengui@gmail.com>
2025-02-26 16:23:00 +0100

    add an handler to managed exception

---
+++ src/main/java/tech/zerofiltre/testing/calcul/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package tech.zerofiltre.testing.calcul.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception e) {
+        return new ResponseEntity<>("An unexpected error occurred : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
