ac7e8d61d387ce5a859c96fc3584b6b0832c073a
Gilles Mbagou <gilles.boussengui@gmail.com>
2025-03-05 16:50:38 +0100

    update controller with new strategy

--- src/main/java/tech/zerofiltre/testing/calcul/controller/CalculatorController.java
+++ src/main/java/tech/zerofiltre/testing/calcul/controller/CalculatorController.java
@@ -3,47 +3,50 @@ package tech.zerofiltre.testing.calcul.controller;
 import javax.inject.Inject;
 import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import tech.zerofiltre.testing.calcul.domain.model.Calculation;
 import tech.zerofiltre.testing.calcul.domain.model.CalculationModel;
 import tech.zerofiltre.testing.calcul.domain.model.CalculationType;
 import tech.zerofiltre.testing.calcul.service.CalculatorService;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:8081/")
 public class CalculatorController {
 
-	public static final String CALCULATOR_TEMPLATE = "calculator";
+	public static final String CALCULATOR_TEMPLATE = "vue-calculator";
 
 	@Inject
 	CalculatorService calculatorService;
 
 	@GetMapping("/")
 	public String index(Calculation calculation) {
-		return "redirect:/calculator";
+		return "redirect:/vue-calculator";
 	}
 
-	@GetMapping("/calculator")
+	@GetMapping("/vue-calculator")
 	public String root(Calculation calculation) {
 		return CALCULATOR_TEMPLATE; // cf. resources/templates/calculator.html
 	}
 
 	@PostMapping("/calculator")
-	public String calculate(@Valid Calculation calculation, BindingResult bindingResult, Model model) {
+	public ResponseEntity<CalculationModel> calculate(@Valid @RequestBody CalculationModel calculation) {
 
-		final CalculationType type = CalculationType.valueOf(calculation.getCalculationType());
-		final CalculationModel calculationModel = new CalculationModel(
-				type,
-				calculation.getLeftArgument(),
-				calculation.getRightArgument());
+		if(calculation.getError() != null) {
+			return ResponseEntity.badRequest().body(calculation);
+		}
+		CalculationModel result = calculatorService.calculate(calculation);
 
-		final CalculationModel response = calculatorService.calculate(calculationModel);
+		return result.getError() == null ?
+				ResponseEntity.ok(result) :
+				ResponseEntity.badRequest().body(result);
 
-		model.addAttribute("response", response);
-		return CALCULATOR_TEMPLATE; // cf. resources/templates/calculator.html
 	}
 }
